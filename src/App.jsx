e0fcbba6a3ed10eaed84166bb786fb99145bc308
kl-16Master <bin843119@gmail.com>
2025-02-04 13:17:38 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import { useEffect, useState } from "react";
-
-function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-        const result = await response.json();
-
-        // Since the response is a single object, wrap it in an array to use map.
-        setData([result]);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-    getData();
-  }, []);
-
-  return (
-    <div>
-      <ul>
-        {data.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+function App (){
+ return
 }
+
+
 
 export default App;
